5c0fc5fd548b2a02984a16c7f9237a40d5386cdc
NoraKrbr <nora.koreuber@mdc-berlin.de>
2024-12-06 16:54:35 +0100

    addressed review comments

--- src/benchmark/simple_segmentation_model.py
+++ src/benchmark/simple_segmentation_model.py
@@ -10,19 +10,26 @@ from transformers import AutoImageProcessor, AutoModel
 class SimpleSegmentationModel(torch.nn.Module):
     """Simple segmentation model that uses a backbone and a linear head."""
     def __init__(self, model_name, num_classes):
-        super(SimpleSegmentationModel, self).__init__()
+        """
+        Initialize the SimpleSegmentationModel with the specified model and number of classes.
+
+        Args:
+            model_name (str): The name of the model to load, one of Prov-GigaPath, Phikon-v2, Virchow2, UNI, Titan.
+            num_classes (int): The number of output classes for the segmentation task.
+        """
+        super().init()
         self.model, self.transform, model_dim = load_model_and_transform(model_name)
         self.head = torch.nn.Linear(model_dim, num_classes, requires_grad=True)
         self.model.eval()
-        self.freeze_backbone()
+        self.freeze_model()
 
-    def freeze_backbone(self):
-        """Freeze the backbone of the model."""
+    def freeze_model(self):
+        """Freeze the model."""
         for param in self.model.parameters():
             param.requires_grad = False
 
-    def unfreeze_backbone(self):
-        """Unfreeze the backbone of the model."""
+    def unfreeze_model(self):
+        """Unfreeze the model."""
         for param in self.model.parameters():
             param.requires_grad = True
 
@@ -34,9 +41,9 @@ class SimpleSegmentationModel(torch.nn.Module):
         Returns:
             logits: Output logits of shape (b, num_classes, h, w)
         """
-        b,c,h,w = x.shape
+        b, c, h, w = x.shape
         x = self.transform(x)
-        patch_embeddings = self.model(x) # 1, 14, 14, 1024
+        patch_embeddings = self.model(x) # output shape (b, p, p, d) where b=batch_size, p=patch_size, d=hidden_dim
         logits= self.head(patch_embeddings)
         logits = torch.nn.functional.interpolate(
             logits, size=(h,w), mode="bilinear", align_corners=False
@@ -55,41 +62,48 @@ def load_model_and_transform(model_name):
         transform (callable): A transform object to prepare input data according to the model's requirements.
         model_dim (int): The dimensionality of the model's output features.
     """
-    if model_name == "Prov-GigaPath":
+    if clean_str(model_name) == "provgigapath":
         model = timm.create_model("hf_hub:prov-gigapath/prov-gigapath", pretrained=True)
         model.forward = lambda x: model.forward_features(x)[:, 1:, :].reshape(x.shape[0], 14, 14, -1)
         transform =  create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
-        with torch.no_grad():
-            model_dim = model(torch.zeros(1, 3, 224, 224)).shape[-1]
-    elif model_name == "Phikon-v2":
+        get_model_dim(model)
+    elif clean_str(model_name) == "phikonv2":
         model = AutoModel.from_pretrained("owkin/phikon-v2")
         model.forward = lambda x: model(x).last_hidden_state[:, 1:, :].reshape(x.shape[0], 14, 14, -1)
         transform = AutoImageProcessor.from_pretrained("owkin/phikon-v2")
-        with torch.no_grad():
-            model_dim = model(torch.zeros(1, 3, 224, 224)).shape[-1]
-    # uncomment next lines for Virchow2 as soon as we are granted access to the huggingface model
-    # elif model_name == "Virchow2":
-    #     model = timm.create_model("hf-hub:paige-ai/Virchow2", pretrained=True, pretrained=True, mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU)
-    #     # start from index 5 of last_hidden_state since first token is CLS and token 1-4 are registers
-    #     model.forward = lambda x: model(x).last_hidden_state[:, 5:, :].reshape(x.shape[0], 16, 16, -1)
-    #     transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
-    #     with torch.no_grad():
-    #         model_dim = model(torch.zeros(1, 3, 224, 224)).shape[-1]
-    elif model_name == "UNI":
+        get_model_dim(model)
+    elif clean_str(model_name) == "virchow2":
+        raise NotImplementedError(f"Access has not been granted to {model_name}") # remove when we have been granted access
+        model = timm.create_model("hf-hub:paige-ai/Virchow2", pretrained=True, pretrained=True, mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU)
+        # start from index 5 of last_hidden_state since first token is CLS and token 1-4 are registers
+        model.forward = lambda x: model(x).last_hidden_state[:, 5:, :].reshape(x.shape[0], 16, 16, -1)
+        transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+        get_model_dim(model)
+    elif clean_str(model_name) == "uni":
         login()
         model = timm.create_model(
             "hf-hub:MahmoodLab/UNI", pretrained=True, init_values=1e-5, dynamic_img_size=True
         )
         model.forward = lambda x: model.forward_features(x)[:, 1:, :].reshape(x.shape[0], 14, 14, -1)
         transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
-        with torch.no_grad():
-            model_dim = model(torch.zeros(1, 3, 224, 224)).shape[-1]
-    elif model_name == "Titan":
+        get_model_dim(model)
+    elif clean_str(model_name) == "titan":
         model = AutoModel.from_pretrained('MahmoodLab/TITAN', trust_remote_code=True)
         conch, transform = model.return_conch()
         model.forward = lambda x: conch._modules['trunk'].forward_features(x)[:,1:,:].reshape(x.shape[0], 14, 14, -1)
-        with torch.no_grad():
-            model_dim = conch(torch.zeros(1, 3, 224, 224)).shape[-1]
+        get_model_dim(model)
     else:
         raise ValueError(f"Unsupported model name: {model_name}")
     return model, transform, model_dim
+
+
+def clean_str(string):
+    return string.replace(" ", "").replace("-", "").lower()
+
+def get_model_dim(model):
+    with torch.no_grad():
+        model_dim = model(torch.zeros(1, 3, 224, 224)).shape[-1]
+    return model_dim
+
+
+    
